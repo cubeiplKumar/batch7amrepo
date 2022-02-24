2f28fb699d04dc65ebb060d59e9be987d38830e1
cubeiplkumar <emailanilkumarm@gmail.com>
2022-02-24 07:54:40 +0530

    feature300 changes done by mahender

--- Application.java
+++ Application.java
@@ -22,4 +22,11 @@ class Main {
       e.getStackTrace();
     }
   }
+  #feature300 new changes done by Developer :Mahender
+  // reads all data from input.txt
+      sourceFile.read(array);
+
+      // writes all data to newFile
+      destFile.write(array);
+      System.out.println("The input.txt file is copied to newFile.");
 }
